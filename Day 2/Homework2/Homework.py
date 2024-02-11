e2907574332d4d6346d72f8f03400ef6808200c3
Kakachia888 <kakachiasandro730@gmail.com>
2024-02-11 10:53:04 -0800

    third commit

---
+++ Day 2/Homework2/Homework.py
@@ -0,0 +1,8 @@
+print("i subscribed to all chanels")
+      
+      
+print("gavicani da davumegbrdi data diasamidzes da gio gagnidzes")
+
+
+
+print("sauketeso saswavlebeli mtel saqartveloshi axlaxan daviwye goashi mushaoba da imedi maqvs damifasdeba is rasac vaketeb da saintereso iqneba goastan tanamshromloba Goa momavalia")      
