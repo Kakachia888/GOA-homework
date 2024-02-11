85c8e586807a8d1d3ea1ac2f52f8d3273aaedcc9
Kakachia888 <kakachiasandro730@gmail.com>
2024-02-11 05:39:42 -0800

    second commit

---
+++ Day 1/Homework/lesson1.py
@@ -0,0 +1,19 @@
+#this is lesson 1
+
+#creating variable of name
+
+name = "sandro"
+
+#creating variable of username 
+
+surname = "Kakachia"
+
+#creating variable of my age
+
+age = 15
+
+#creating variable of my height
+
+Height = 1.69
+
+print(" my name is " + name + " my surname is " +surname+ " my age is " +str(age) + " my height is "+str(Height))
